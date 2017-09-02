ebc804f56caa2c8b35b0e4df427ad01868f28f09
creepy-coder <triocomfort@gmail.com>
2017-09-03 00:12:31 +0300

    fixed views commit

--- app/Post.php
+++ app/Post.php
@@ -3,15 +3,55 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Like;
 
 class Post extends Model
 {
     //
     protected $fillable = ['title', 'body', 'user_id', 'image'];
+    protected $with = ['user', 'likes'];
 
-    public function user(){
+    public function user()
+    {
 
         return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'liked');
+    }
+
+    public function like()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if(! $this->likes()->where($attributes)->exists()){
+
+            return $this->likes()->create($attributes);
+        }
+    }
+    public function unLike()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if($this->likes()->where($attributes)->exists()){
+
+            return $this->likes()->delete($attributes);
+        }
+    }
+    public function isLiked()
+    {
+        return !! $this->likes->where('user_id', auth()->id())->count();
+    }
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    public function canEdit()
+    {
+        return $this->likes->first()->user_id;
     }
 
 }
--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Authorizable;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +28,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class)->latest();
+    }
 }
